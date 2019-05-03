27b5123076bbd8e409ab0a8e181664cc09990ffd
Ian Maulana <ian_maulana@yahoo.com>
2019-05-03 13:54:08 +0700

    build dev

---
+++ build/precache-manifest.5dfc5b7fe2ebfe842b397590cc730c92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90714018387d8cd3de92c08ea6c14103",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c02ac91173c7d89ad583",
+    "url": "/static/css/main.7ba1fbc0.chunk.css"
+  },
+  {
+    "revision": "bef5b55b06676a2b5462",
+    "url": "/static/js/2.617cd546.chunk.js"
+  },
+  {
+    "revision": "c02ac91173c7d89ad583",
+    "url": "/static/js/main.9e094339.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
